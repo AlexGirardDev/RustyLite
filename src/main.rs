c8d2211c1458bc058072f50a3982bcad327ce70d
Alex Girard <me@alexgirard.ca>
2023-10-14 19:16:21 -0400

    moving to typed header file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{bail, Result};
+use std::default;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -17,22 +18,81 @@ fn main() -> Result<()> {
         ".dbinfo" => {
             let mut file = File::open(&args[1])?;
             let mut header = [0; 100];
-            file.read_exact(&mut header,)?;
+            file.read_exact(&mut header)?;
 
             let page_size = u16::from_be_bytes([header[16], header[17]]);
             println!("Logs from your program will appear here!");
 
-            let mut b_tree_header = [0; 8];
-            file.read_exact(&mut b_tree_header)?;
+            let page = Page::new(file);
 
-            let num_of_rows = u16::from_be_bytes([b_tree_header[3], b_tree_header[4]]);
             println!("Logs from your program will appear here!");
 
             println!("database page size: {}", page_size);
-            println!("number of tables: {}", num_of_rows);
+            println!("number of tables: {}", page.page_header.cell_count);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
 
     Ok(())
+}
+
+impl Page {
+    fn new(mut file: File) -> Self {
+        let mut buffer = [0; 1];
+        file.read_exact(&mut buffer).unwrap();
+        println!("{:?}", buffer);
+        let page_type = match u8::from_be_bytes(buffer) {
+            2 => PageType::InteriorIndex,
+            5 => PageType::InteriorTable,
+            10 => PageType::LeafIndex,
+            13 => PageType::LeafTable,
+            _ => panic!("invalid page type"),
+        };
+
+        let mut buffer = [0; 2];
+
+        file.read_exact(&mut buffer).unwrap();
+        let free_block = u16::from_be_bytes(buffer);
+
+        file.read_exact(&mut buffer).unwrap();
+        let cell_count = u16::from_be_bytes(buffer);
+
+        file.read_exact(&mut buffer).unwrap();
+        let cell_content_area = u16::from_be_bytes(buffer);
+
+        let mut buffer = [0; 1];
+        file.read_exact(&mut buffer).unwrap();
+        let fragmented_free_bytes = u8::from_be_bytes(buffer);
+
+        Page {
+            page_header: PageHeader {
+                page_type,
+                free_block,
+                cell_count,
+                cell_content_area,
+                fragmented_free_bytes,
+                right_pointer: None,
+            },
+        }
+    }
+}
+
+struct Page {
+    page_header: PageHeader,
+}
+
+struct PageHeader {
+    page_type: PageType,
+    free_block: u16,
+    cell_count: u16,
+    cell_content_area: u16,
+    fragmented_free_bytes: u8,
+    right_pointer: Option<u32>,
+}
+
+enum PageType {
+    InteriorIndex,
+    InteriorTable,
+    LeafIndex,
+    LeafTable,
 }
